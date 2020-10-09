5c399074a7558b50258c974a4b5084fbb0815fbd
Josees Bronner <joseesbronnner2@gmail.com>
2020-10-09 09:52:34 -0400

    Project 1

---
+++ MVP/app.js
@@ -0,0 +1,19 @@
+// MVP for prject 1
+Jeopardy 
+1. From the title screen you will be able to pick how many people you want to play ranging from 1-4
+2. Once you select the number of players round 1 will start with a jeopard board (wireframe) 
+3. player 1 will be able to answer first then player 2 and so on
+   - the next player cannot pick the same box that a previous player has already picked 
+4. when you click on a box it will take you a question and give you 3 different items you can only use once during the game 
+	- a stike item: takes away one option
+	- time item: give you more time
+	- double points: gives you double points if you get the question right 
+5. the game consist of 3 rounds 
+6. the first two rounds consist of 5 questions and the last round is just 1
+7. after the first round the amount of points you can get goes up
+8. the last round is a bonus round where you pick how many of your points you want to bet on and if you 
+	get the question right you get that many amount of points and if you dont you lose that amount of points 
+
+- game shows each players points at the top
+- game shows who turn it is 
+- 
